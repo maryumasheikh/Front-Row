29d4b5e6414262802bba8e70470016b0f8319f84
maryumasheikh <maryum.a.sheikh@gmail.com>
2026-07-16 22:14:37 -0500

    initial commit

--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="nativewind/types" />
+
+declare const process: {
+  env: {
+    EXPO_PUBLIC_APPWRITE_DATABASE_ID: string;
+    EXPO_PUBLIC_APPWRITE_COLLECTION_ID: string;
+    EXPO_PUBLIC_APPWRITE_PROJECT_ID: string;
+    EXPO_PUBLIC_MOVIE_API_KEY: string;
+  };
+};
