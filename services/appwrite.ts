18556e2308736802e1bda5597a9ff4914d629743
maryumasheikh <maryum.a.sheikh@gmail.com>
2026-07-16 23:20:09 -0500

    working on fixing Appwrite client

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -3,9 +3,10 @@ import { Client, Databases, ID, Query } from "react-native-appwrite";
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
-const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+const client = (new Client()
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!) as any)
+  .setPlatform("com.mas.frontrow");
 
 const database = new Databases(client);
 
